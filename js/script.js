4bec2fdb5f8e18c67b6dd81e02d68d862b3679e6
Edward <83923244+TesvintsevDev@users.noreply.github.com>
2022-10-08 22:32:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-// let burger = document.querySelector('.burger')
-// let menu = document.querySelector('.header-nav')
-// let menuLinks = menu.querySelectorAll('.header-nav__link')
-
-// burger.addEventListener('click',
-
-// function() {
-//   burger.classList.toggle('burger--active')
-
-//   menu.classList.toggle('header-nav--active')
-
-//   document.body.classList.toggle('stop-scroll')
-// })
-
-// menuLinks.forEach(function(el) {
-//   el.addEventListener('click', function() {
-
-//     burger.classList.remove('burger--active')
-
-//     menu.classList.remove('header-nav__active')
-
-//     document.body.classList.remove('stop-scroll')
-//   })
-// })
-
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
 
